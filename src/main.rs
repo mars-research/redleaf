7fe7ddfe89a5a800a1deed50f2ccb5bb3c2c2574
Anton Burtsev <aburtsev@uci.edu>
2019-08-22 13:26:37 -0700

    Initial baremetal Rust
    -- This is based on https://os.phil-opp.com/freestanding-rust-binary/

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    main();
+    loop {}
+}
+
+fn main() {
+    // println!("Hello, world!");
+}
