78e5db9aee30391c1d1c5da1a076e7e43a2abe49
Tianjiao Huang <tianjih1@uci.edu>
2019-10-26 15:21:18 -0600

    add comment to member functions in log

--- src/filesystem/log.rs
+++ src/filesystem/log.rs
@@ -42,14 +42,19 @@ impl Log {
         return log;
     }
 
+    // Copy committed blocks from log to their home location
     fn install_trans(&mut self) {
 
     }
 
+    // Read the log header from disk into the in-memory log header
     fn read_head(&mut self) {
 
     }
 
+    // Write in-memory log header to disk.
+    // This is the true point at which the
+    // current transaction commits.
     fn write_head(&mut self) {
 
     }
@@ -58,14 +63,18 @@ impl Log {
 
     }
 
+    // called at the start of each FS system call.
     fn begin_op(&mut self) {
 
     }
 
+    // called at the end of each FS system call.
+    // commits if this was the last outstanding operation.
     fn end_op(&mut self) {
 
     }
 
+    // Copy modified blocks from cache to log.
     fn write_log(&mut self) {
 
     }
@@ -74,8 +83,17 @@ impl Log {
 
     }
 
+    // Caller has modified b->data and is done with the buffer.
+    // Record the block number and pin in the cache by increasing refcnt.
+    // commit()/write_log() will do the disk write.
+    //
+    // log_write() replaces bwrite(); a typical use is:
+    //   bp = bread(...)
+    //   modify bp->data[]
+    //   log_write(bp)
+    //   brelse(bp)
     fn log_write(&mut self) {
-        
+
     }
 
 
