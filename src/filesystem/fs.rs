3925b066c708f2f4192f2f78a5b1539d43884bd7
Dan Appel <dan.appel00@gmail.com>
2019-11-08 10:48:53 -0800

    Implement ilock() as INode.update()

--- src/filesystem/fs.rs
+++ src/filesystem/fs.rs
@@ -1,6 +1,7 @@
 use alloc::sync::Arc;
 use spin::Mutex;
 use core::mem::MaybeUninit;
+use core::sync::atomic::{AtomicBool, Ordering};
 use crate::filesystem::params;
 use crate::filesystem::bcache::{BCACHE, BufferBlock};
 
@@ -26,7 +27,7 @@ pub struct INodeMeta {
     // Inode number
     pub inum: u32,
     // inode has been read from disk?
-    pub valid: i32,
+    pub valid: AtomicBool,
 }
 
 #[repr(C)]
@@ -48,7 +49,10 @@ pub struct INodeData {
 pub type DINode = INodeData;
 
 impl INode {
-    fn update(&self) {
+    // Copy a modified in-memory inode to disk (ie flush)
+    // Call after every modification to Inode.data
+    // xv6 equivalent: iupdate()
+    fn flush(&self) {
         let super_block = get_super_block();
 
         let data = self.data.lock();
@@ -70,8 +74,48 @@ impl INode {
         dinode.addresses.copy_from_slice(&data.addresses);
 
         // TODO: log_write
+
         drop(buffer);
         BCACHE.release(&mut bguard);
+    }
+
+    // Reads from disk if necessary
+    // xv6 equivalent: ilock(...)
+    fn update(&self) {
+        let super_block = get_super_block();
+
+        if self.meta.valid.load(Ordering::Relaxed) {
+            return;
+        }
+
+        // if not valid, load from disk
+        let mut data = self.data.lock();
+
+        let mut bguard = BCACHE.read(self.meta.device, block_num_for_node(self.meta.inum, &super_block));
+        let buffer = bguard.lock();
+
+        // TODO: work around unsafe
+        let dinode = unsafe {
+            & *(&buffer.data as *const BufferBlock as *mut BufferBlock as *mut DINode)
+                .offset((self.meta.inum % params::IPB as u32) as isize)
+        };
+
+        data.file_type = dinode.file_type;
+        data.major = dinode.major;
+        data.minor = dinode.minor;
+        data.nlink = dinode.nlink;
+        data.size = dinode.size;
+        data.addresses.copy_from_slice(&dinode.addresses);
+
+        drop(buffer);
+        BCACHE.release(&mut bguard);
+
+        self.meta.valid.store(true, Ordering::Relaxed);
+
+        if dinode.file_type == 0 {
+            // TODO: better error handling here
+            panic!("ilock: no type");
+        }
     }
 }
 
@@ -86,7 +130,7 @@ impl INode {
             meta: INodeMeta {
                 device: 0,
                 inum: 0,
-                valid: 0
+                valid: AtomicBool::new(false)
             },
             data: Mutex::new(INodeData {
                 file_type: 0,
@@ -121,7 +165,9 @@ impl ICache {
         }
     }
 
-    pub fn alloc(&mut self, device: u32, file_type: i16) -> Arc<INode> {
+    // Allocate a node on device.
+    // Looks for a free inode on disk, marks it as used
+    pub fn alloc(&mut self, device: u32, file_type: i16) -> Option<Arc<INode>> {
         let super_block = get_super_block();
         for inum in 1..super_block.ninodes {
             let mut bguard = BCACHE.read(device, block_num_for_node(inum, &super_block));
@@ -131,9 +177,10 @@ impl ICache {
             let mut dinode = unsafe {
                 &mut *(&buffer.data as *const BufferBlock as *mut BufferBlock as *mut DINode).offset((inum % params::IPB as u32) as isize)
             };
-            if dinode.file_type == 0 {
+            if dinode.file_type == 0 { // free inode
                 // memset to 0
                 unsafe { core::ptr::write_bytes(dinode as *const DINode as *mut DINode, 0, 1); }
+                // setting file_type marks it as used
                 dinode.file_type = file_type;
                 // TODO: log_write here
                 drop(buffer);
@@ -143,14 +190,15 @@ impl ICache {
             drop(buffer);
             BCACHE.release(&mut bguard);
         }
-        panic!("ialloc: no inodes");
+        None
     }
 
-    pub fn get(&mut self, device: u32, inum: u32) -> Arc<INode> {
+    // Get in-memory inode matching device and inum. Does not read from disk.
+    pub fn get(&mut self, device: u32, inum: u32) -> Option<Arc<INode>> {
         let mut empty: Option<&mut Arc<INode>> = None;
         for inode in self.inodes.iter_mut() {
             if Arc::strong_count(inode) == 1 && inode.meta.device == device && inode.meta.inum == inum {
-                return inode.clone();
+                return Some(inode.clone());
             }
             if empty.is_none() && Arc::strong_count(inode) == 1 {
                 empty = Some(inode);
@@ -158,14 +206,14 @@ impl ICache {
         }
 
         match empty {
-            None => panic!("iget: no inodes"),
+            None => return None,
             Some(node) => {
                 // we just checked that strong_count == 1, and self is locked, so this should never fail
                 let node_mut = Arc::get_mut(node).unwrap();
                 node_mut.meta.device = device;
                 node_mut.meta.inum = inum;
-                node_mut.meta.valid = false as i32;
-                return node.clone();
+                node_mut.meta.valid.store(false, Ordering::Relaxed);
+                return Some(node.clone());
             }
         }
     }
