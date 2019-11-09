8fe0c0a5b65789315c7cc2e2e48f06d433ff6d5b
Tianjiao Huang <maitian519@gmail.com>
2019-11-08 17:52:02 -0800

    begin/end_op

--- src/filesystem/log.rs
+++ src/filesystem/log.rs
@@ -42,7 +42,7 @@ pub struct Log {
     start: u32,
     size: u32,
     outstanding: u32, // how many FS sys calls are executing.
-    committing: u32,  // in commit(), please wait.
+    committing: bool,  // in commit(), please wait.
     dev: u32,
     logheader: LogHeader,
 }
@@ -57,7 +57,7 @@ impl Log {
             start: superblock.logstart,
             size: superblock.nlog,
             outstanding: 0,
-            committing: 0,
+            committing: false,
             dev,
             logheader: LogHeader{
                 n: 123456,
@@ -111,14 +111,46 @@ impl Log {
     // A better implementation of this function would be that
     // this functions returns a guard that can be used to write_log.
     // And end_op will be called when the guard is dropped.
-    fn try_begin_op(&mut self) {
-        unimplemented!();
+    // TODO(tianjiao): fix this
+    fn try_begin_op(&mut self) -> bool {
+        if self.committing {
+            return false;
+        }
+        if self.logheader.n + (self.outstanding+1)*params::MAXOPBLOCKS as u32 > params::LOGSIZE as u32 {
+            return false;
+        }
+        self.outstanding += 1;
+        return true;
     }
 
     // called at the end of each FS system call.
     // commits if this was the last outstanding operation.
     // Caller should repeatly call this function until this function returns true.
     fn try_end_op(&mut self) {
+        let do_commit: bool = false;
+
+        self.outstanding -= 1;
+        assert!(!self.committing, "log.commiting");
+        if self.outstanding == 0 {
+            do_commit = true;
+            self.committing = true;
+        } else {
+            // begin_op() may be waiting for log space,
+            // and decrementing log.outstanding has decreased
+            // the amount of reserved space.
+            // wakeup(&log);
+        }
+
+        // This part is EXTREMELY inefficient and weird, but it should work
+        // Basically we just hold the lock till we finish commiting.
+        if do_commit {
+            // call commit w/o holding locks, since not allowed
+            // to sleep with locks.
+            // TODO(tianjiao): fix this
+            self.commit();
+            self.committing = false;
+            // wakeup(&log);
+        }
         unimplemented!();
     }
 
