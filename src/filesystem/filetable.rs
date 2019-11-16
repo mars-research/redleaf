c84d57f88e8e5fd05fbe2bcf9b7fec20c82040b8
Dan Appel <dan.appel00@gmail.com>
2019-11-16 13:57:18 -0800

    Implement read, use Arc<Mutex<File> (not ideal)

--- src/filesystem/filetable.rs
+++ src/filesystem/filetable.rs
@@ -2,22 +2,21 @@ use crate::filesystem::fs::{ICache, INode, Stat};
 use crate::filesystem::params;
 use alloc::sync::Arc;
 use core::mem::{MaybeUninit, swap};
-use spin::{Mutex, RwLock};
+use spin::Mutex;
 
 pub enum FileType {
-    None,
     Pipe, // TODO: { pipe: Arc<Pipe> }
-    INode { inode: Arc<INode>, offset: u32 },
+    INode { inode: Arc<INode>, offset: usize },
     Device { inode: Arc<INode>, major: i16 },
 }
 
 pub struct File {
-    pub file_type: FileType,
+    pub file_type: Option<FileType>,
     pub readable: bool,
     pub writable: bool,
 }
 
-pub type FileHandle = Arc<RwLock<File>>;
+pub type FileHandle = Arc<Mutex<File>>;
 
 pub struct FileTable {
     files: Mutex<[FileHandle; params::NFILE]>,
@@ -26,7 +25,7 @@ pub struct FileTable {
 impl File {
     pub fn new() -> File {
         File {
-            file_type: FileType::None,
+            file_type: None,
             readable: false,
             writable: false
         }
@@ -37,9 +36,9 @@ impl FileTable {
     pub fn new() -> FileTable {
         FileTable {
             files: Mutex::new(unsafe {
-                let mut arr = MaybeUninit::<[Arc<RwLock<File>>; params::NFILE]>::uninit();
+                let mut arr = MaybeUninit::<[FileHandle; params::NFILE]>::uninit();
                 for i in 0..params::NFILE {
-                    (arr.as_mut_ptr() as *mut Arc<RwLock<File>>).add(i).write(Arc::new(RwLock::new(File::new())));
+                    (arr.as_mut_ptr() as *mut FileHandle).add(i).write(Arc::new(Mutex::new(File::new())));
                 }
                 arr.assume_init()
             })
@@ -63,32 +62,53 @@ impl FileTable {
             return
         }
 
-        let mut file_type = FileType::None;
+        // <=2 references, ie this pointer and the ftable's pointer, so actually close the file
 
-        {
-            // <=2 references, ie this pointer and the ftable's pointer, so actually close the file
-            let mut fguard = file.write();
+        let file_type = { file.lock().file_type.take() };
 
-            // set file's type to None (since we are closing it), pull out file_type
-            swap(&mut fguard.file_type, &mut file_type);
-        }
-
-        match file_type {
-            FileType::INode { inode, .. } | FileType::Device { inode, .. } => {
-                // TODO: log begin_op here
-                ICache::put(inode);
-                // TODO: log end_op here
-            },
-            // TODO: pipe
-            _ => ()
+        if let Some(file_type) = file_type {
+            match file_type {
+                FileType::INode { inode, .. } | FileType::Device { inode, .. } => {
+                    // TODO: log begin_op here
+                    ICache::put(inode);
+                    // TODO: log end_op here
+                },
+                // TODO: pipe
+                _ => ()
+            }
         }
     }
 
     // xv6 equivalent: filestat
     pub fn stat(&self, file: FileHandle) -> Option<Stat> {
-        match &file.read().file_type {
-            FileType::INode { inode, .. } | FileType::Device { inode, .. } => Some(inode.lock().stat()),
+        match &file.lock().file_type {
+            Some(FileType::INode { inode, .. }) | Some(FileType::Device { inode, .. }) => Some(inode.lock().stat()),
             _ => None
+        }
+    }
+
+    // Reads bytes from file into user_buffer.
+    // Returns number of bytes read, or None if lacking read permissions or upon overflow.
+    // xv6 equivalent: fileread
+    pub fn read(&self, file: FileHandle, user_buffer: &mut [u8]) -> Option<usize> {
+        let mut fguard = file.lock();
+
+        if !fguard.readable {
+            return None;
+        }
+
+        match &mut fguard.file_type {
+            Some(FileType::INode { inode, offset }) => {
+                let mut iguard = inode.lock();
+                if let Some(bytes) = iguard.read(user_buffer, *offset) {
+                    *offset += bytes;
+                    return Some(bytes);
+                } else {
+                    return None;
+                }
+            },
+            // TODO: device, pipe
+            _ => unimplemented!()
         }
     }
 }
