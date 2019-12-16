d514126dcfa7e14f0845a4bd35a384f491920cc3
Vikram Narayanan <vikram186@gmail.com>
2019-12-15 22:27:52 -0800

    Add userland allocator and alloc/free syscalls

--- src/syscalls.rs
+++ src/syscalls.rs
@@ -2,6 +2,9 @@ use crate::interrupt::{disable_irq, enable_irq};
 use crate::thread::{do_yield, create_thread};
 use usr::capabilities::Capability;
 use syscalls::syscalls::Syscall;
+use x86::bits64::paging::{PAddr, VAddr};
+use crate::arch::vspace::VSpace;
+use crate::memory::paddr_to_kernel_vaddr;
 
 // Print a string 
 pub fn sys_print(s: &str) {
@@ -11,6 +14,20 @@ pub fn sys_print(s: &str) {
     enable_irq(); 
 }
 
+pub fn sys_alloc() -> *mut u8 {
+    disable_irq();
+    let paddr: PAddr = VSpace::allocate_one_page();
+    let vaddr: VAddr = paddr_to_kernel_vaddr(paddr);
+    println!("sys_alloc: returning {:x}", vaddr.as_u64());
+    enable_irq();
+    vaddr.as_mut_ptr()
+}
+
+// TODO: Implement free!
+pub fn sys_free(_p: *mut u8) {
+    disable_irq();
+    enable_irq();
+}
 
 // Yield to any thread
 pub fn sys_yield() {
@@ -31,10 +48,10 @@ pub fn sys_create_thread(name: &str, func: extern fn()) -> Capability  {
     return cap;
 }
 
-
 pub static UKERN: Syscall = Syscall{
     sys_print,
     sys_yield,
     sys_create_thread,
+    sys_alloc,
+    sys_free,
 };
-
--- sys/lib/syscalls/src/syscalls.rs
+++ sys/lib/syscalls/src/syscalls.rs
@@ -8,6 +8,8 @@ pub struct Syscall {
     pub sys_print: fn(s: &str),
     pub sys_yield: fn(),
     pub sys_create_thread: fn(name: &str, func: extern fn()) -> Capability,
+    pub sys_alloc: fn() -> *mut u8,
+    pub sys_free: fn(p: *mut u8),
 }
 
 pub fn init(s: Syscall) {
@@ -29,3 +31,12 @@ pub fn sys_create_thread(name: &str, func: extern fn()) -> Capability {
     return (scalls.sys_create_thread)(name, func);
 }
 
+pub fn sys_alloc() -> *mut u8 {
+    let scalls = SYSCALL.r#try().expect("System call interface is not initialized.");
+    return (scalls.sys_alloc)();
+}
+
+pub fn sys_free(p: *mut u8) {
+    let scalls = SYSCALL.r#try().expect("System call interface is not initialized.");
+    return (scalls.sys_free)(p);
+}
