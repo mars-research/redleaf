e21673f02e9f2752efee5f58a5c94d6ab371046f
Anton Burtsev <aburtsev@uci.edu>
2019-12-18 14:15:57 -0800

    Remove old usr

--- usr/src/capabilities.rs
+++
@@ -1,2 +0,0 @@
-
-pub type Capability = u64;
--- usr/src/syscalls.rs
+++
@@ -1,9 +0,0 @@
-use crate::capabilities::Capability; 
-
-#[derive(Copy, Clone)]
-pub struct Syscall {
-    pub sys_print: fn(s: &str),
-    pub sys_yield: fn(),
-    pub sys_create_thread: fn(name: &str, func: extern fn()) -> Capability,
-}
-
