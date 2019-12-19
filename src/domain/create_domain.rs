d710a5162a6ec1ce40eec22207313a9ad04b6e59
Zhaofeng Li <hello@zhaofeng.li>
2019-12-19 15:50:24 -0800

    Add common create_domain()

---
+++ src/domain/create_domain.rs
@@ -0,0 +1,42 @@
+use elfloader::ElfBinary;
+use super::Domain;
+use alloc::string::String;
+use crate::syscalls::UKERN;
+use syscalls::syscalls::Syscall;
+use core::mem::transmute;
+use crate::interrupt::{disable_irq, enable_irq};
+
+pub unsafe fn create_domain(name: &'static str, binary_range: (*const u8, *const u8)) {
+    let (binary_start, binary_end) = binary_range;
+    type user_init = fn(Syscall);
+
+    let num_bytes = ((binary_end as usize) - (binary_start as usize)) as usize;
+
+    println!("domain/{}: Binary start: {:x}, end: {:x} ", name, binary_start as usize, binary_end as usize);
+
+    // Create a new elf binary from the address range we just extracted
+    let sys_init_elf = ElfBinary::new(name, core::slice::from_raw_parts(binary_start, num_bytes)).expect("Invalid ELF file");
+
+    // Create a domain for the to-be-loaded elf file
+    let mut loader = Domain::new(String::from(name));
+
+    // load the binary
+    sys_init_elf.load(&mut loader).expect("Cannot load binary");
+
+    // print its entry point for now
+    println!("domain/{}: Entry point at {:x}", name, loader.offset + sys_init_elf.entry_point());
+
+    let user_ep: user_init = unsafe {
+        let mut entry: *const u8 = loader.offset.as_ptr();
+        entry = entry.offset(sys_init_elf.entry_point() as isize);
+        let _entry = entry as *const ();
+        transmute::<*const(), user_init>(_entry)
+    };
+
+    // Enable interrupts as we do upcall into user
+    enable_irq();
+    user_ep(UKERN);
+    disable_irq(); 
+
+    println!("domain/{}: Returned", name);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -63,7 +63,7 @@ use crate::interrupt::{enable_irq};
 use crate::syscalls::UKERN;
 use crate::memory::construct_pt;
 use crate::pci::scan_pci_devs;
-use crate::domain::sys_init::load_sys_init;
+use crate::domain::create_domain;
 
 #[no_mangle]
 pub static mut cpu1_stack: u32 = 0;
@@ -108,10 +108,19 @@ pub fn init_allocator() {
 }
 
 fn init_user() {
-    //crate::thread::create_thread("init", usr::init::init); 
-    //usr::init::init(UKERN); 
-    
-    unsafe { load_sys_init(); }
+    extern "C" {
+        fn _binary_sys_init_build_init_start();
+        fn _binary_sys_init_build_init_end();
+    }
+
+    let binary_range = (
+        _binary_sys_init_build_init_start as *const u8,
+        _binary_sys_init_build_init_end as *const u8
+    );
+
+    unsafe {
+        create_domain("sys_init", binary_range);
+    }
 }
 
 const MAX_CPUS: u32 = 32;
