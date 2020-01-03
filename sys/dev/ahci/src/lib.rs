0ff49b890168a31f966846dc2082647a6b6e412f
Zhaofeng Li <hello@zhaofeng.li>
2020-01-03 13:46:17 -0800

    Quick BDev implementation

--- sys/dev/ahci/src/lib.rs
+++ sys/dev/ahci/src/lib.rs
@@ -27,18 +27,27 @@ use syscalls::{Syscall};
 use libsyscalls::syscalls::{sys_print, sys_alloc};
 use console::println;
 use alloc::boxed::Box;
+use alloc::vec::Vec;
 
-struct AHCI {}
+struct AHCI {
+    disk: Box<dyn self::ahcid::Disk>,
+}
 
 impl AHCI {
-    fn new() -> AHCI {
-        AHCI{}
+    fn new(disk: Box<dyn self::ahcid::Disk>) -> AHCI {
+        AHCI {
+            disk: disk,
+        }
     }
 }
 
 impl syscalls::BDev for AHCI {
-    fn read(self, block: u32, data: &mut [u8; 512]) { unimplemented!() }
-    fn write(self, block: u32, data: &[u8; 512]) { unimplemented!() }
+    fn read(&mut self, block: u32, data: &mut [u8; 512]) {
+        self.disk.read(block as u64, data);
+    }
+    fn write(&mut self, block: u32, data: &[u8; 512]) {
+        self.disk.write(block as u64, data);
+    }
 }
 
 
@@ -47,10 +56,10 @@ pub fn ahci_init(s: Box<dyn Syscall + Send + Sync>,
                  pci: Box<dyn syscalls::PCI>) -> Box<dyn syscalls::BDev> {
     libsyscalls::syscalls::init(s);
 
-    //self::ahcid::disks(0xfebf1000, "meow");
+    let (hba, mut disks) = self::ahcid::disks(0xfebf1000, "meow");
 
-    println!("ahci_init: starting AHCI domain");
-    Box::new(AHCI::new()) 
+    println!("ahci_init: Started AHCI domain");
+    Box::new(AHCI::new(disks.remove(0))) 
 }
 
 // This function is called on panic.
