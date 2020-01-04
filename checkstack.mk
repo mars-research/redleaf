55636bd770a419835ee6ce29c166c9ec57bc8efb
Anton Burtsev <aburtsev@uci.edu>
2020-01-03 21:33:33 -0800

    Better error messages for stack usage

--- checkstack.mk
+++ checkstack.mk
@@ -5,14 +5,12 @@ checkstack: checkstackinfo
 
 .PHONY: checkstackinfo
 checkstackinfo:
-	@echo  thread.rs: $(root)/src/thread.rs
-	@echo  bin: $(bin)
 	$(eval max_stack := $(shell stack-sizes $(bin) | sort -k2 -nr | awk 'NR==1{print $$2}'))
 	$(eval half_ukern_stack := $(shell grep "^const STACK_SIZE_IN_PAGES" $(root)/src/thread.rs | grep -o '[[:digit:]]*' | awk '{print $$1*4096/2}'))
 	$(eval max_stacks := $(shell stack-sizes $(bin) | sort -k2 -nr | head -n 1))
-	@echo "Max allocated stack used by this domain: $(max_stack), half of kernel stack: $(half_ukern_stack), function:" 
+	@echo "Max allocated stack used by this domain is $(max_stack) bytes, which is less than half of kernel stack ($(half_ukern_stack) bytes)"
+	@echo "The largest stack is allocated by this function:" 
 	@echo "$(max_stacks)"
-	@echo "Use"
-	@echo "\>stack-sizes $(kernel)"
-	@echo "for more info"
+	@echo "You can use this command for detailed info on stack usage in this domain"
+	@echo "    \>stack-sizes $(bin) | sort -k2 -nr"
 
