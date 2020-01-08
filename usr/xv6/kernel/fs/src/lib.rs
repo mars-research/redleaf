d1e2c177d8f4116c9716e1892df09264e61b94d6
Tianjiao Huang <maitian519@gmail.com>
2020-01-07 16:33:59 -0800

    add fs_benchmark

--- usr/xv6/kernel/fs/src/lib.rs
+++ usr/xv6/kernel/fs/src/lib.rs
@@ -28,9 +28,11 @@ extern crate tls;
 
 use alloc::boxed::Box;
 use alloc::string::String;
+use alloc::vec::Vec;
 use console::println;
 use core::panic::PanicInfo;
 use syscalls::Syscall;
+use libsyscalls::time::get_ns_time;
 
 mod bcache;
 mod block;
@@ -61,9 +63,26 @@ pub fn init(s: Box<dyn Syscall + Send + Sync>, bdev: syscalls::BDevPtr) -> Box<d
     println!("init xv6 filesystem");
     fs::fsinit(0);
     println!("finish init xv6 filesystem");
-    ls("/").unwrap();
-
+    // ls("/").unwrap();
+    fs_benchmark(512, "/big_file");
     Box::new(VFS::new()) 
+}
+
+fn fs_benchmark(buf_size: usize, path: &str) {
+    let start = get_ns_time();
+    let fd = sysfile::sys_open(path, sysfile::FileMode::Read).unwrap();
+    let mut buff = Vec::new();
+    buff.resize(buf_size, 0 as u8);
+    let mut bytes_read = 0;
+    while let Some(sz) = sysfile::sys_read(fd, buff.as_mut_slice()) {
+        bytes_read += sz;
+        if sz < 512 {
+            break;
+        }
+    }
+    sysfile::sys_close(fd);
+    let end = get_ns_time();
+    println!("we read {} bytes at a time, in total {} bytes from {} using {} cycles", buf_size, bytes_read, path, end - start);
 }
 
 fn ls(path: &str) -> Result<(), String> {
