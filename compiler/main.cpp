41f6269dd72295c6d9794a55779aed41a0b52c6d
David Detweiler <detweild@uci.edu>
2020-01-11 21:19:55 -0800

    Add argument type collection to compiler

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -5,26 +5,31 @@
 
 int main(int argc, char** argv)
 {
+    bool is_good {true};
+    const compiler::data* data;
     try {
-        bool is_good {true};
-        auto data = static_cast<const compiler::data*>(Parser::parse(std::string {argv[1]}));
-        for (const std::size_t id : data->type_refs) {
-            const auto type_i = std::find(data->allowed_types.begin(), data->allowed_types.end(), id);
-            if (type_i == data->allowed_types.end()) {
-                std::printf("Could not resolve referenced type \"%s\"\n", data->identifiers.at(id).c_str());
-                is_good = false;
-            }
-        }
-        if (is_good)
-            std::printf("Everything looks good!\n");
-
-        std::printf("init() used these traits (return type last):\n");
-        for (const std::size_t id : data->init_signature) {
-            std::printf("%s\n", data->identifiers.at(id).c_str());
+        data =static_cast<const compiler::data*>(Parser::parse(std::string {argv[1]}));
+    } catch (const Parser::ParseException& e) {
+        std::printf("%s", e.getReason().c_str());
+        return -1;
+    }
+    for (const std::size_t id : data->type_refs) {
+        const auto type_i = std::find(data->allowed_types.begin(), data->allowed_types.end(), id);
+        if (type_i == data->allowed_types.end()) {
+            std::printf("Could not resolve referenced type \"%s\"\n", data->identifiers.at(id).c_str());
+            is_good = false;
         }
     }
-    catch (const Parser::ParseException& e) {
-        std::printf("%s", e.getReason().c_str());
+    if (is_good)
+        std::printf("Everything looks good!\n");
+
+    for (const auto arg : data->arguments) {
+        std::printf("Argument \"%s\" of type %s\n", data->identifiers.at(arg.name).c_str(), arg.type.c_str());
+    }
+
+    std::printf("init() used these traits (return type last):\n");
+    for (const std::size_t id : data->init_signature) {
+        std::printf("%s\n", data->identifiers.at(id).c_str());
     }
 
     return 0;
--- compiler/parse_data.h
+++ compiler/parse_data.h
@@ -3,10 +3,22 @@
 #include <string>
 
 namespace compiler {
+    struct free_function {
+        std::size_t name;
+        std::size_t arg_list;
+        std::size_t num_args;
+    };
+
+    struct argument {
+        std::size_t name;
+        std::string type;
+    };
+
     struct data {
         std::vector<std::string> identifiers;
+        std::vector<argument> arguments;
         std::vector<std::size_t> allowed_types;
-        std::vector<std::size_t> type_refs;
+        std::vector<std::size_t> type_refs;        
         std::vector<std::size_t> init_signature;
     };
 }
