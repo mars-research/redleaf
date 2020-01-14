65e67f14b8c0c32452a3545976e3c242691a3f29
David Detweiler <detweild@uci.edu>
2020-01-13 18:27:05 -0800

    Add full call information collection

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -1,16 +1,31 @@
 #include <algorithm>
+#include <iterator>
+#include <cstring>
 
 #include "parse_data.h"
 #include "parser.h"
 
-int main(int argc, char** argv)
+int main(int argc, const char** argv)
 {
+    if (argc < 2)
+        return -1;
+    
+    bool dbg_enabled {false};
+    auto path {argv[1]};
+
+    if (argc == 3) {
+        if (std::strcmp(argv[1], "dbg") != 0)
+            return -1;
+        dbg_enabled = true;
+        path = argv[2];
+    }
+
     bool is_good {true};
     const compiler::data* data;
     try {
-        data =static_cast<const compiler::data*>(Parser::parse(std::string {argv[1]}));
+        data =static_cast<const compiler::data*>(Parser::parse(std::string {path}));
     } catch (const Parser::ParseException& e) {
-        std::printf("%s", e.getReason().c_str());
+        std::printf("%s\n", e.getReason().c_str());
         return -1;
     }
     for (const std::size_t id : data->type_refs) {
@@ -20,11 +35,46 @@ int main(int argc, char** argv)
             is_good = false;
         }
     }
-    if (is_good)
-        std::printf("Everything looks good!\n");
 
-    for (const auto arg : data->arguments) {
-        std::printf("Argument \"%s\" of type %s\n", data->identifiers.at(arg.name).c_str(), arg.type.c_str());
+    if (!is_good)
+        return -1;
+
+    std::printf("Everything looks good!\n");
+
+    if (!dbg_enabled)
+        return 0;
+
+    for (const auto& func : data->free_functions) {
+        const auto name = data->identifiers.at(func.name).c_str();
+        auto arg_i = std::next(data->arguments.begin(), func.arg_list);
+        const auto args_end = std::next(arg_i, func.num_args);
+        std::printf("Free function \"%s\" with arguments:\n", name);
+        for (; arg_i < args_end; ++arg_i) {
+            const auto& arg = *arg_i;
+            const auto arg_name = data->identifiers.at(arg.name).c_str();
+            const auto arg_type = arg.type.c_str();
+            std::printf("\t\"%s\" of type %s\n", arg_name, arg_type);
+        }
+    }
+
+    for (const auto& trait : data->traits) {
+        const auto name = data->identifiers.at(trait.name).c_str();
+        auto fn_i = std::next(data->member_functions.begin(), trait.fn_list);
+        const auto fns_end = std::next(fn_i, trait.num_fns);
+        std::printf("Trait \"%s\" with member functions:\n", name);
+        for (; fn_i < fns_end; ++fn_i) {
+            const auto& func = *fn_i;
+            const auto name = data->identifiers.at(func.name).c_str();
+            auto arg_i = std::next(data->arguments.begin(), func.arg_list);
+            const auto args_end = std::next(arg_i, func.num_args);
+            std::printf("\t\"%s\" with arguments:\n", name);
+            for (; arg_i < args_end; ++arg_i) {
+                const auto& arg = *arg_i;
+                const auto arg_name = data->identifiers.at(arg.name).c_str();
+                const auto arg_type = arg.type.c_str();
+                std::printf("\t\t\"%s\" of type %s\n", arg_name, arg_type);
+            }
+        }
     }
 
     std::printf("init() used these traits (return type last):\n");
--- compiler/parse_data.h
+++ compiler/parse_data.h
@@ -3,10 +3,16 @@
 #include <string>
 
 namespace compiler {
-    struct free_function {
+    struct function {
         std::size_t name;
         std::size_t arg_list;
         std::size_t num_args;
+    };
+
+    struct trait {
+        std::size_t name;
+        std::size_t fn_list;
+        std::size_t num_fns;
     };
 
     struct argument {
@@ -17,6 +23,9 @@ namespace compiler {
     struct data {
         std::vector<std::string> identifiers;
         std::vector<argument> arguments;
+        std::vector<function> free_functions;
+        std::vector<function> member_functions;
+        std::vector<trait> traits;
         std::vector<std::size_t> allowed_types;
         std::vector<std::size_t> type_refs;        
         std::vector<std::size_t> init_signature;
