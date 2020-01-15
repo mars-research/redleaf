5f71739a022d9297e3a93b0a50e543964bd600a2
Dan Appel <dan.appel00@gmail.com>
2020-01-14 21:58:18 -0800

    Implement domain changes

--- src/heap.rs
+++ src/heap.rs
@@ -7,7 +7,8 @@ use crate::memory::MEM_PROVIDER;
 use syscalls::Heap;
 
 // usize == *mut u8
-static allocations: Mutex<Vec<(u64, usize, Layout)>> = Mutex::new(Vec::new());
+static mut allocations: Vec<(u64, usize, Layout)> = Vec::new();
+static alloc_lock: Mutex<()> = Mutex::new(());
 
 pub struct PHeap();
 
@@ -68,14 +69,18 @@ impl Heap for PHeap {
 
 fn alloc_heap(domain_id: u64, layout: Layout) -> *mut u8 {
     let ptr = unsafe { MEM_PROVIDER.alloc(layout) };
-    allocations.lock().push((domain_id, ptr as usize, layout));
+    {
+        let lock = alloc_lock.lock();
+        unsafe { &mut allocations }.push((domain_id, ptr as usize, layout));
+        drop(lock);
+    }
     ptr
 }
 
 fn dealloc_heap(domain_id: u64, ptr: *mut u8, layout: Layout) {
-    allocations.lock().retain(|(a_domain_id, a_ptr, a_layout) | {
+    let lock = alloc_lock.lock();
+    unsafe { &mut allocations }.retain(|(a_domain_id, a_ptr, a_layout) | {
         // only dealloc if it's been allocated in the same way and owned by the calling domain
-        // TODO: track domain owner changes
         if domain_id == *a_domain_id && ptr == *a_ptr as *mut u8 && layout == *a_layout {
             unsafe { MEM_PROVIDER.dealloc(ptr, layout) }
             false
@@ -83,10 +88,12 @@ fn dealloc_heap(domain_id: u64, ptr: *mut u8, layout: Layout) {
             true
         }
     });
+    drop(lock);
 }
 
 fn change_domain(from_domain_id: u64, to_domain_id: u64, ptr: *mut u8, layout: Layout) {
-    allocations.lock().iter_mut().map(|(a_domain_id, a_ptr, a_layout)| {
+    // TODO: this is lockless and unproved
+    unsafe { &mut allocations }.iter_mut().map(|(a_domain_id, a_ptr, a_layout)| {
         if from_domain_id == *a_domain_id && ptr == *a_ptr as *mut u8 && layout == *a_layout {
             *a_domain_id = to_domain_id;
         }
@@ -94,7 +101,8 @@ fn change_domain(from_domain_id: u64, to_domain_id: u64, ptr: *mut u8, layout: L
 }
 
 fn drop_domain(domain_id: u64) {
-    allocations.lock().retain(|(a_domain_id, a_ptr, a_layout)| {
+    let lock = alloc_lock.lock();
+    unsafe { &mut allocations }.retain(|(a_domain_id, a_ptr, a_layout)| {
         if domain_id == *a_domain_id {
             unsafe { MEM_PROVIDER.dealloc(*a_ptr as *mut u8, *a_layout) }
             false
@@ -102,4 +110,5 @@ fn drop_domain(domain_id: u64) {
             true
         }
     });
+    drop(lock);
 }
