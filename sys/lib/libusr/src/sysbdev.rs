3cd5d7832d014145640835e2a399f7f86dcc7286
Dan Appel <dan.appel00@gmail.com>
2020-01-15 17:55:07 -0800

    Use force_get for proxy bdev interfaces

--- sys/lib/libusr/src/sysbdev.rs
+++ sys/lib/libusr/src/sysbdev.rs
@@ -5,31 +5,31 @@ use alloc::boxed::Box;
 use rref::RRef;
 
 pub fn sys_new_data(data: [u8; 512]) -> RRef<[u8; 512]> {
-    let proxy = PROXY.r#try().expect("Proxy interface is not initialized.");
+    let proxy = PROXY.force_get();//.r#try().expect("Proxy interface is not initialized.");
     proxy.bdev_new_data(data)
 }
 
 pub fn sys_drop_data(data: RRef<[u8; 512]>) {
-    let proxy = PROXY.r#try().expect("Proxy interface is not initialized.");
+    let proxy = PROXY.force_get();//.r#try().expect("Proxy interface is not initialized.");
     proxy.bdev_drop_data(data)
 }
 
 pub fn sys_read(block: u32, data: &mut RRef<[u8; 512]>) {
-    let proxy = PROXY.r#try().expect("Proxy interface is not initialized.");
+    let proxy = PROXY.force_get();//.r#try().expect("Proxy interface is not initialized.");
     proxy.bdev_read(block, data)
 }
 
 pub fn sys_write(block: u32, data: &[u8; 512]) {
-    let proxy = PROXY.r#try().expect("Proxy interface is not initialized.");
+    let proxy = PROXY.force_get();//.r#try().expect("Proxy interface is not initialized.");
     proxy.bdev_write(block, data)
 }
 
 pub fn sys_foo() {
-    let proxy = PROXY.r#try().expect("Proxy interface is not initialized.");
+    let proxy = PROXY.force_get();//.r#try().expect("Proxy interface is not initialized.");
     proxy.bdev_foo()
 }
 
 pub fn sys_bar(data: &mut RRef<[u8; 512]>) {
-    let proxy = PROXY.r#try().expect("Proxy interface is not initialized.");
+    let proxy = PROXY.force_get();//.r#try().expect("Proxy interface is not initialized.");
     proxy.bdev_bar(data)
 }
