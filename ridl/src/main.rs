6fbe3996ce871f0caef9c315f5817bb6c2333501
David Detweiler <detweild@uci.edu>
2020-02-17 10:48:32 -0800

    Add working signature dump

--- ridl/src/main.rs
+++ ridl/src/main.rs
@@ -13,12 +13,47 @@ struct Arg {
     typ: String
 }
 
-fn get_type(ty: syn::Type) -> String {
+fn get_path(p: &syn::Path) -> String {
+    let mut t = String::new();
+    let mut colon = match p.leading_colon {
+        Some(_) => true,
+        None => false
+    };
+    for seg in &p.segments {
+        if colon {
+            t += "::";
+        }
+        t += &seg.ident.to_string();
+        match &seg.arguments {
+            syn::PathArguments::None => (),
+            syn::PathArguments::AngleBracketed(args) => {
+                t += "<";
+                let mut comma = false;
+                for arg in &args.args {
+                    if comma {
+                        t += ", "
+                    }
+                    comma = true;
+                    match arg {
+                        syn::GenericArgument::Type(nt) => t += &get_type(nt),
+                        _ => {println!("[ERROR] Non-type generic arguments not supported"); panic!()}
+                    }
+                }
+                t += ">"
+            },
+            _ => {println!("[ERROR] Must be angle-bracketed generic"); panic!()}
+        }
+        colon = true;
+    }
+    t
+}
+
+fn get_type(ty: &syn::Type) -> String {
     let mut t = String::new();
     match ty {
         syn::Type::Reference(r) => {
             t += "&";
-            match r.lifetime {
+            match &r.lifetime {
                 Some(v) => {t += "'"; t += &v.ident.to_string(); t += " "},
                 None => ()
             }
@@ -26,64 +61,63 @@ fn get_type(ty: syn::Type) -> String {
                 Some(_) => t += "mut ",
                 None => ()
             }
-            t += &get_type(*r.elem)
+            t += &get_type(&*r.elem)
         },
         syn::Type::Array(a) => {
             t += "[";
-            t += &get_type(*a.elem);
+            t += &get_type(&*a.elem);
             t += "; ";
-            t += &match a.len {
-                syn::Expr::Lit(l) => match l.lit {
+            t += &match &a.len {
+                syn::Expr::Lit(l) => match &l.lit {
                     syn::Lit::Int(i) => i,
                     _ => panic!()
                 },
-                _ => {println!("NOT LITERAL"); panic!()}
+                _ => {println!("[ERROR] Must be literal size"); panic!()}
             }.to_string();
             t += "]"
         },
         syn::Type::Path(p) => {
-            let mut colon = match p.path.leading_colon {
-                Some(_) => true,
-                None => false
-            };
-            for seg in p.path.segments {
-                if colon {
-                    t += "::";
+            t += &get_path(&p.path)
+        },
+        syn::Type::Tuple(tup) => {
+            t += "(";
+            let mut comma = false;
+            for item in &tup.elems {
+                if comma {
+                    t += ", "
                 }
-                t += &seg.ident.to_string();
-                match seg.arguments {
-                    syn::PathArguments::None => (),
-                    syn::PathArguments::AngleBracketed(args) => {
-                        t += "<";
-                        let mut comma = false;
-                        for arg in args.args {
-                            if comma {
-                                t += ", "
-                            }
-                            comma = true;
-                            match arg {
-                                syn::GenericArgument::Type(nt) => t += &get_type(nt),
-                                _ => {println!("NOT A TYPE"); panic!()}
-                            }
-                        }
-                        t += ">"
-                    },
-                    _ => {println!("NOT AB"); panic!()}
-                }
-                colon = true;
+                comma = true;
+                t += &get_type(&item)
+            }
+            t += ")"
+        },
+        syn::Type::TraitObject(tr) => {
+            match &tr.dyn_token {
+                Some(_) => t += "dyn ",
+                None => ()
+            }
+            if tr.bounds.len() != 1 {
+                println!("[ERROR] Multiple traits not allowed");
+                panic!()
+            }
+            match &tr.bounds[0] {
+                syn::TypeParamBound::Trait(tr) => t += &get_path(&tr.path),
+                _ => {println!("[ERROR] Must be a trait"); panic!()}
             }
         },
-        syn::Type::Tuple(_) => print!("Strange Type: "),
-        syn::Type::TraitObject(_) => print!("Strange Type: "),
-        syn::Type::Slice(_) => print!("Strange Type: "),
-        _ => {println!("ILLEGAL TYPE!!"); panic!()}
+        syn::Type::Slice(s) => {
+            t += "[";
+            t += &get_type(&*s.elem);
+            t += "]"
+        },
+        _ => {println!("[ERROR] This type not allowed"); panic!()}
     }
     t
 }
 
 fn process_typed(typed: syn::PatType) -> Arg {
     match *typed.pat {
-        syn::Pat::Ident(id) => Arg {name: id.ident.to_string(), typ: get_type(*typed.ty)},
+        syn::Pat::Ident(id) => Arg {name: id.ident.to_string(), typ: get_type(&*typed.ty)},
         _ => panic!()
     }
 }
@@ -95,13 +129,19 @@ fn process_arg(arg: syn::FnArg) -> Arg {
     }
 }
 
-fn extract(ast: syn::File) {
+fn extract(ast: syn::File) -> (Vec<Scope>, Vec<Scope>, Vec<Arg>) {
     let whitelist = vec!["Syscall", "Heap"];
+    let mut args: Vec<Arg> = Vec::new();
+    let mut funcs: Vec<Scope> = Vec::new();
+    let mut traits: Vec<Scope> = Vec::new();
     for item in ast.items {
         let tr = match item {
             syn::Item::Trait(tr) => tr,
             _ => continue
         };
+
+        let mut trscope = Scope {name: tr.ident.to_string(), start: funcs.len(), end: 0};
+
         let mut in_white = false;
         for w in &whitelist {
             if tr.ident.to_string() == *w {
@@ -112,17 +152,29 @@ fn extract(ast: syn::File) {
         if in_white {
             continue
         }
+
         for item in tr.items {
             let func = match item {
                 syn::TraitItem::Method(m) => m,
                 _ => continue
             };
+            let mut fscope = Scope {name: func.sig.ident.to_string(), start: args.len(), end: 0};
             for item in func.sig.inputs {
-                let arg = process_arg(item);
-                println!("{} {}", arg.name, arg.typ)
+                args.push(process_arg(item))
             }
+            let rt = match func.sig.output {
+                syn::ReturnType::Type(_, ty) => get_type(&ty),
+                _ => "()".to_string()
+            };
+            args.push(Arg {name: "__rt".to_string(), typ: rt});
+            fscope.end = args.len();
+            funcs.push(fscope)
         }
+
+        trscope.end = funcs.len();
+        traits.push(trscope);
     }
+    (traits, funcs, args)
 }
 
 fn main() {
@@ -141,5 +193,14 @@ fn main() {
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
     let ast = syn::parse_file(&content).unwrap();
-    extract(ast);
+    let (traits, funcs, args) = extract(ast);
+    for tr in traits {
+        println!("{}", tr.name);
+        for i in tr.start .. tr.end {
+            println!("\t{}", funcs[i].name);
+            for j in funcs[i].start .. funcs[i].end {
+                println!("\t\t{}: {}", args[j].name, args[j].typ)
+            }
+        }
+    }
 }
