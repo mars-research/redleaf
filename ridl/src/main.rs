a77e6e84d9a759012275bf35599de68dc68c9b2c
David Detweiler <detweild@uci.edu>
2020-02-23 13:38:19 -0800

    Add #[derive(Copy)] tracking

--- ridl/src/main.rs
+++ ridl/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 use std::env;
+use proc_macro2;
 
 struct Scope {
     name: String,
@@ -10,7 +11,7 @@ struct Scope {
 
 struct Arg {
     name: String,
-    typ: String
+    ty: String
 }
 
 fn get_path(p: &syn::Path, known: &Vec<String>) -> String {
@@ -141,7 +142,7 @@ fn get_type(ty: &syn::Type, top: bool, known: &Vec<String>) -> String {
 
 fn process_typed(typed: syn::PatType, known: &Vec<String>) -> Arg {
     match *typed.pat {
-        syn::Pat::Ident(id) => Arg {name: id.ident.to_string(), typ: get_type(&*typed.ty, true, known)},
+        syn::Pat::Ident(id) => Arg {name: id.ident.to_string(), ty: get_type(&*typed.ty, true, known)},
         _ => panic!()
     }
 }
@@ -149,8 +150,38 @@ fn process_typed(typed: syn::PatType, known: &Vec<String>) -> Arg {
 fn process_arg(arg: syn::FnArg, known: &Vec<String>) -> Arg {
     match arg {
         syn::FnArg::Typed(a) => process_typed(a, known),
-        syn::FnArg::Receiver(_) => Arg {name: "self".to_string(), typ: String::new()}
+        syn::FnArg::Receiver(_) => Arg {name: "self".to_string(), ty: String::new()}
     }
+}
+
+fn is_copy(tokens: proc_macro2::TokenStream) -> bool {
+    for tok in tokens {
+        match tok {
+            proc_macro2::TokenTree::Group(g) => {
+                for tok in g.stream() {
+                    match tok {
+                        proc_macro2::TokenTree::Ident(id) => {
+                            if id == "Copy" {
+                                return true
+                            }
+                        },
+                        _ => ()
+                    }
+                }
+            },
+            _ => panic!()
+        }
+    }
+    false
+}
+
+fn process_attrs(attrs: Vec<syn::Attribute>) -> bool {
+    for attr in attrs {
+        if attr.path.segments.len() == 1 && attr.path.segments[0].ident == "derive" {
+            return is_copy(attr.tokens);
+        }
+    }
+    false
 }
 
 fn extract(ast: syn::File) -> (Vec<Scope>, Vec<Scope>, Vec<Arg>) {
@@ -168,12 +199,16 @@ fn extract(ast: syn::File) -> (Vec<Scope>, Vec<Scope>, Vec<Arg>) {
             },
             syn::Item::Enum(e) => {
                 println!("Found enum {}", e.ident);
-                known.push(e.ident.to_string());
+                if process_attrs(e.attrs) {
+                    known.push(e.ident.to_string());
+                }
                 continue
             },
             syn::Item::Struct(e) => {
                 println!("Found struct {}", e.ident);
-                known.push(e.ident.to_string());
+                if process_attrs(e.attrs) {
+                    known.push(e.ident.to_string());
+                }
                 continue
             },
             syn::Item::TraitAlias(e) => {
@@ -189,7 +224,7 @@ fn extract(ast: syn::File) -> (Vec<Scope>, Vec<Scope>, Vec<Arg>) {
             syn::Item::Use(_) => {
                 println!("Found use decl (WARNING: incomplete handling)");
                 continue
-            }
+            },
             _ => continue
         };
 
@@ -219,7 +254,7 @@ fn extract(ast: syn::File) -> (Vec<Scope>, Vec<Scope>, Vec<Arg>) {
                 syn::ReturnType::Type(_, ty) => get_type(&ty, true, &known),
                 _ => "()".to_string()
             };
-            args.push(Arg {name: "__rt".to_string(), typ: rt});
+            args.push(Arg {name: "__rt".to_string(), ty: rt});
             fscope.end = args.len();
             funcs.push(fscope)
         }
@@ -252,7 +287,7 @@ fn main() {
         for i in tr.start .. tr.end {
             println!("\t{}", funcs[i].name);
             for j in funcs[i].start .. funcs[i].end {
-                println!("\t\t{}: {}", args[j].name, args[j].typ)
+                println!("\t\t{}: {}", args[j].name, args[j].ty)
             }
         }
     }
