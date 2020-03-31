91d428bcbe5b490746a6ba3b2f3436b54090b179
Vikram Narayanan <vikram186@gmail.com>
2020-03-30 19:01:48 -0600

    bar: Map 1MiB of bar space

--- sys/dev/pci/src/lib.rs
+++ sys/dev/pci/src/lib.rs
@@ -90,7 +90,7 @@ impl syscalls::PCI for PCI {
 
         println!("Getting bar region {:x}", bar);
 
-        let bar_region = pci_bar.get_bar_region(bar as u64, 512 * 1024 as usize, pci_driver.get_driver_type());
+        let bar_region = pci_bar.get_bar_region(bar as u64, 1024 * 1024 as usize, pci_driver.get_driver_type());
 
         println!("Probing device...");
 
