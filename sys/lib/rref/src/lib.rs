d582ca004a14fae2d03e291a507f0f75d745bfc5
Dan Appel <dan.appel00@gmail.com>
2020-04-23 00:41:19 -0700

    Implement RRefDeque

--- sys/lib/rref/src/lib.rs
+++ sys/lib/rref/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![feature(array_value_iter)]
+#![feature(const_generics)]
 extern crate alloc;
 use core::ops::{Deref, DerefMut, Drop};
 use core::alloc::Layout;
@@ -22,6 +23,133 @@ struct SharedHeapObject<T> where T: 'static {
 impl<T> Drop for SharedHeapObject<T> {
     fn drop(&mut self) {
         panic!("SharedHeapObject::drop should never be called.");
+    }
+}
+
+pub struct RRefDeque<T, const N: usize> where T: 'static {
+    arr: RRef<[Option<T>; N]>,
+    head: usize, // index of the next element that can be written
+    tail: usize, // index of the first element that can be read
+}
+
+impl<T, const N: usize> RRefDeque<T, N> {
+    pub fn new(empty_arr: [Option<T>; N]) -> Self {
+        Self {
+            arr: RRef::new(empty_arr),
+            head: 0,
+            tail: 0
+        }
+    }
+
+    pub fn push_back(&mut self, value: T) {
+        if self.head == self.tail && self.arr[self.head].is_some() {
+            // if overwriting tail, push tail back
+            self.tail = (self.tail + 1) % N;
+        }
+        self.arr[self.head] = Some(value);
+        self.head = (self.head + 1) % N;
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let value = self.arr[self.tail].take();
+        if value.is_some() {
+            self.tail = (self.tail + 1) % N;
+        }
+        return value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate alloc;
+    use alloc::vec::Vec;
+    use core::mem;
+    use syscalls::{Syscall, Thread};
+
+    pub struct TestHeap();
+
+    impl TestHeap {
+        pub fn new() -> TestHeap {
+            TestHeap {}
+        }
+    }
+
+    impl syscalls::Heap for TestHeap {
+        unsafe fn alloc(&self, _: u64, layout: Layout) -> *mut u8 {
+            let mut buf = Vec::with_capacity(layout.size());
+            let ptr = buf.as_mut_ptr();
+            mem::forget(buf);
+            ptr
+        }
+
+        unsafe fn dealloc(&self, _: u64, _: *mut u8, _: Layout) {}
+
+        unsafe fn change_domain(&self, _: u64, _: u64, _: *mut u8, _: Layout) {}
+    }
+
+    pub struct TestSyscall();
+    impl TestSyscall {
+        pub fn new() -> Self { Self {} }
+    }
+    impl Syscall for TestSyscall {
+        fn sys_print(&self, s: &str) {}
+        fn sys_println(&self, s: &str) {}
+        fn sys_cpuid(&self) -> u32 { 0 }
+        fn sys_yield(&self) {}
+        fn sys_create_thread(&self, name: &str, func: extern fn()) -> Box<dyn Thread> { panic!() }
+        fn sys_current_thread(&self) -> Box<dyn Thread> { panic!() }
+        fn sys_get_current_domain_id(&self) -> u64 { 0 }
+        unsafe fn sys_update_current_domain_id(&self, new_domain_id: u64) -> u64 { 0 }
+        fn sys_alloc(&self) -> *mut u8 { panic!() }
+        fn sys_free(&self, p: *mut u8) { }
+        fn sys_alloc_huge(&self, sz: u64) -> *mut u8 { panic!() }
+        fn sys_free_huge(&self, p: *mut u8) {}
+        fn sys_backtrace(&self) {}
+        fn sys_dummy(&self) {}
+    }
+
+    fn init_heap() {
+        init(Box::new(TestHeap::new()));
+    }
+    fn init_syscall() {
+        libsyscalls::syscalls::init(Box::new(TestSyscall::new()));
+    }
+
+    #[test]
+    fn rrefdeque_empty() {
+        init_heap();
+        init_syscall();
+        let mut deque = RRefDeque::<usize, 3>::new(Default::default());
+        assert_eq!(deque.pop_front(), None);
+    }
+
+    #[test]
+    fn rref_deque_insertion() {
+        init_heap();
+        init_syscall();
+        let mut deque = RRefDeque::<usize, 3>::new(Default::default());
+        deque.push_back(1);
+        deque.push_back(2);
+        assert_eq!(deque.pop_front(), Some(1));
+        assert_eq!(deque.pop_front(), Some(2));
+    }
+
+    #[test]
+    fn rref_deque_overrite() {
+        init_heap();
+        init_syscall();
+        let mut deque = RRefDeque::<usize, 3>::new(Default::default());
+        deque.push_back(1);
+        deque.push_back(2);
+        deque.push_back(3);
+        deque.push_back(4);
+        assert_eq!(deque.pop_front(), Some(2));
+        deque.push_back(5);
+        assert_eq!(deque.pop_front(), Some(3));
+        assert_eq!(deque.pop_front(), Some(4));
+        assert_eq!(deque.pop_front(), Some(5));
+        assert_eq!(deque.pop_front(), None);
     }
 }
 
