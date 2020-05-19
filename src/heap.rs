f3b76623bd94eb0d43f433e63ed70d20398b4324
Dan Appel <dan.appel00@gmail.com>
2020-05-18 18:25:07 -0700

    Use hashmap for shared heap

--- src/heap.rs
+++ src/heap.rs
@@ -4,7 +4,9 @@ use core::alloc::{GlobalAlloc, Layout};
 use spin::Mutex;
 use crate::interrupt::{disable_irq, enable_irq};
 use crate::memory::MEM_PROVIDER;
+use hashbrown::HashMap;
 
+#[derive(Copy, Clone)]
 struct SharedHeapAllocation {
     ptr: *mut u8, // *mut T
     domain_id_ptr: *mut u64,
@@ -12,8 +14,12 @@ struct SharedHeapAllocation {
     drop_fn: extern fn(*mut u8) -> (), // semantically Drop::<T>::drop
 }
 
-static mut allocations: Vec<SharedHeapAllocation> = Vec::new();
-static alloc_lock: Mutex<()> = Mutex::new(());
+unsafe impl Send for SharedHeapAllocation {}
+unsafe impl Sync for SharedHeapAllocation {}
+
+lazy_static! {
+    static ref allocations: Mutex<HashMap<usize, SharedHeapAllocation>> = Mutex::new(HashMap::new());
+}
 
 pub struct PHeap();
 
@@ -41,51 +47,43 @@ impl syscalls::Heap for PHeap {
 unsafe fn alloc_heap(layout: Layout, drop_fn: extern fn(*mut u8) -> ()) -> (*mut u64, *mut u8) {
     let domain_id_ptr = MEM_PROVIDER.alloc(Layout::new::<u64>()) as *mut u64;
     let ptr = MEM_PROVIDER.alloc(layout);
-    {
-        let lock = alloc_lock.lock();
 
-        unsafe { &mut allocations }.push(SharedHeapAllocation {
-            ptr,
-            domain_id_ptr,
-            layout,
-            drop_fn,
-        });
-    }
+    unsafe { &mut allocations.lock() }.insert(ptr as usize, SharedHeapAllocation {
+        ptr,
+        domain_id_ptr,
+        layout,
+        drop_fn,
+    });
+
     (domain_id_ptr, ptr)
 }
 
 unsafe fn dealloc_heap(ptr: *mut u8) {
-    let lock = alloc_lock.lock();
-
-    // TODO: drop one object, instead of looping through all of them via retain
-    (&mut allocations).retain(|allocation | {
-        if ptr == allocation.ptr {
-            // TODO: drop domain_ptr
-            unsafe { MEM_PROVIDER.dealloc(ptr, allocation.layout) }
-            false
-        } else {
-            true
+    if let Some(allocation) = unsafe { &mut allocations.lock() }.remove(&(ptr as usize)) {
+        unsafe {
+            MEM_PROVIDER.dealloc(ptr, allocation.layout);
+            MEM_PROVIDER.dealloc(allocation.domain_id_ptr as *mut u8, Layout::new::<u64>());
         }
-    });
-
-    drop(lock);
+    }
 }
 
 pub unsafe fn drop_domain(domain_id: u64) {
-    let mut lock = Some(alloc_lock.lock());
     // remove all allocations from list that belong to the exited domain
-    (&mut allocations).retain(|allocation| {
-        let this_domain_id = *(allocation.domain_id_ptr);
-        if domain_id == this_domain_id {
-            drop(&mut lock.take());
-            (allocation.drop_fn)(allocation.ptr);
-            lock.replace(alloc_lock.lock());
-            // TODO: drop domain_ptr
-            unsafe { MEM_PROVIDER.dealloc(allocation.ptr, allocation.layout) }
-            false
-        } else {
-            true
+    let mut alloc_maps = allocations.lock();
+    let mut queue = Vec::<SharedHeapAllocation>::new();
+    for key in alloc_maps.keys() {
+        let allocation = alloc_maps.get(key).unwrap();
+        if *(allocation.domain_id_ptr) == domain_id {
+            queue.push(*allocation);
         }
-    });
-    drop(lock);
+    }
+    drop(alloc_maps);
+    for allocation in queue.iter() {
+        (allocation.drop_fn)(allocation.ptr);
+
+        unsafe {
+            MEM_PROVIDER.dealloc(allocation.ptr, allocation.layout);
+            MEM_PROVIDER.dealloc(allocation.domain_id_ptr as *mut u8, Layout::new::<u64>());
+        }
+    }
 }
