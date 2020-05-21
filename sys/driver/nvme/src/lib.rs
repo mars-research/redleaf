511dc9affff98b83118aed9654e629c4e21a8f94
Tianjiao Huang <tianjiao@node-0.vikram-qv68928.xcap.emulab.net>
2020-05-21 16:27:16 -0600

    nvme: refactor run_blocktest_rref to libbenchnvme

--- sys/driver/nvme/src/lib.rs
+++ sys/driver/nvme/src/lib.rs
@@ -344,164 +344,6 @@ fn perf_test_iov(dev: &Nvme, runtime: u64, batch_sz: u64, is_write: bool) {
     }
 }
 
-fn run_blocktest_rref(dev: &Nvme, block_sz: usize, is_write: bool, is_random: bool)
-{
-    let mut block_num: u64 = 0;
-    let batch_sz = 32;
-    let runtime = 30;
-
-    let mut data = [0u8; 4096];
-    if is_write {
-        data = [0xeeu8; 4096];
-    }
-
-    let mut submit = RRefDeque::<BlkReq, 128>::default();
-    let mut collect = RRefDeque::<BlkReq, 128>::default();
-    let mut poll = RRefDeque::<BlkReq, 1024>::default();
-
-    for i in 0..32 {
-        let mut breq = BlkReq::from_data(data.clone());
-        if is_random {
-            breq.block = get_rand_block();
-        } else {
-            breq.block = block_num;
-            block_num = block_num.wrapping_add(8);
-        }
-        submit.push_back(RRef::<BlkReq>::new(breq));
-    }
-
-    println!("======== Starting {}{} test (rrefs)  ==========",
-                                    if is_random { "rand" } else { "" },
-                                    if is_write { "write" } else { "read" });
-
-    if let Some(device) = dev.device.borrow_mut().as_mut() {
-        let dev: &mut NvmeDev = device;
-
-        let mut submit_start = 0;
-        let mut submit_elapsed = 0;
-
-        let mut alloc_count = 0;
-        let mut alloc_elapsed = 0;
-
-        let mut count: u64 = 0;
-
-        let mut submit = Some(submit);
-        let mut collect = Some(collect);
-        let mut poll = Some(poll);
-
-        let mut submit_hist = Base2Histogram::new();
-        let mut poll_hist = Base2Histogram::new();
-        let mut ret = 0;
-        let mut last_cq = 0;
-        let mut last_sq = 0;
-        let mut sq_id = 0;
-
-        let tsc_start = rdtsc();
-        let tsc_end = tsc_start + runtime * 2_400_000_000;
-
-        loop {
-            count += 1;
-            submit_start = rdtsc();
-            let (ret, _last_sq, _last_cq, _sq_id, mut submit_, mut collect_) = dev.submit_and_poll_rref(submit.take().unwrap(),
-                                                 collect.take().unwrap(), is_write);
-            submit_elapsed += rdtsc() - submit_start;
-
-            if ret > 0 {
-                last_sq = _last_sq;
-            }
-
-            if collect_.len() > 0 {
-                last_cq = _last_cq;
-                sq_id = _sq_id;
-            }
-
-            //println!("submitted {} reqs, collect {} reqs sq: {} cq {}", ret, collect_.len(), last_sq, last_cq);
-            submit_hist.record(ret as u64);
-
-            poll_hist.record(collect_.len() as u64);
-
-            while let Some(mut breq) = collect_.pop_front() {
-                if is_random {
-                    breq.block = get_rand_block();
-                } else {
-                    breq.block = block_num;
-                    block_num = block_num.wrapping_add(8);
-                }
-                if submit_.push_back(breq).is_some() {
-                    println!("submit too full already!");
-                    break;
-                }
-            }
-
-            if submit_.len() == 0  {//&& (alloc_count * batch_sz) < 1024 {
-                //println!("Alloc new batch at {}", count);
-                alloc_count += 1;
-                let alloc_rdstc_start = rdtsc();
-                for i in 0..batch_sz {
-                    let mut breq = BlkReq::from_data(data.clone());
-                    if is_random {
-                        breq.block = get_rand_block();
-                    } else {
-                        breq.block = block_num;
-                        block_num = block_num.wrapping_add(8);
-                    }
-                    submit_.push_back(RRef::<BlkReq>::new(breq));
-                }
-                alloc_elapsed += rdtsc() - alloc_rdstc_start;
-            }
-
-
-            submit.replace(submit_);
-            collect.replace(collect_);
-
-            if rdtsc() > tsc_end {
-                break;
-            }
-            //sys_ns_loopsleep(2000);
-        }
-
-        let elapsed = rdtsc() - tsc_start;
-
-        let adj_runtime = elapsed as f64 / 2_400_000_000_u64 as f64;
-
-        let (sub, comp) = dev.get_stats();
-
-        println!("Polling .... last_sq {} last_cq {} sq_id {}", last_sq, last_cq, sq_id);
-
-        let (done, poll_) = dev.poll_rref(poll.take().unwrap());
-
-        println!("Poll: Reaped {} requests", done);
-
-        if let Some(mut submit) = submit.take() {
-            println!("submit {} requests", submit.len());
-        }
-
-        if let Some(mut collect) = collect.take() {
-            println!("collect {} requests", collect.len());
-        }
-        println!("runtime: {:.2} seconds", adj_runtime);
-
-        println!("submitted {:.2} K IOPS completed {:.2} K IOPS",
-                             sub as f64 / adj_runtime as f64 / 1_000 as f64,
-                             comp as f64 / adj_runtime as f64 / 1_000 as f64);
-        println!("submit_and_poll_rref took {} cycles (avg {} cycles)",
-                                            submit_elapsed, submit_elapsed / count);
-
-        println!("Number of new allocations {}", alloc_count * batch_sz);
-
-
-        for hist in alloc::vec![submit_hist, poll_hist] {
-            println!("hist:");
-            // Iterate buckets that have observations
-            for bucket in hist.iter().filter(|b| b.count > 0) {
-                print!("({:5}, {:5}): {}", bucket.start, bucket.end, bucket.count);
-                print!("\n");
-            }
-        }
-        println!("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-    }
-}
-
 fn run_blocktest_raw(dev: &Nvme, runtime: u64, batch_sz: u64, is_write: bool) {
 
     let mut req: Vec<u8>;
@@ -672,27 +514,6 @@ fn run_blocktest(dev: &Nvme, runtime: u64, batch_sz: u64, is_write: bool) {
     }
 }
 
-static mut seed: u64 = 123456789;
-static pow: u64 = 2u64.pow(31);
-
-fn get_rand_block() -> u64 {
-    unsafe {
-        seed = (1103515245 * seed + 12345) % pow;
-        seed % 781422768
-    }
-}
-
-fn rand_test(num_iter: usize) -> u64 {
-    let mut sum = 0u64;
-    for _ in 0..num_iter {
-        let rand = get_rand_block();
-        //println!("rand {}", rand);
-        sum += rand;
-    }
-    sum
-}
-
-
 #[no_mangle]
 pub fn nvme_init(s: Box<dyn Syscall + Send + Sync>,
                  heap: Box<dyn Heap + Send + Sync>,
@@ -712,24 +533,24 @@ pub fn nvme_init(s: Box<dyn Syscall + Send + Sync>,
     {
         let num_iter = 10_000_000;
         let rand_start = rdtsc();
-        let sum = rand_test(num_iter);
+        let sum = libbenchnvme::rand_test(num_iter);
         let rand_elapsed = rdtsc() - rand_start;
         println!("Rand {} test {} iterations took {} cycles (avg {} cycles)", sum, num_iter,
                                         rand_elapsed, rand_elapsed as f64 / num_iter as f64);
     }
 
-    run_blocktest_rref(&nvme, 4096, false, false);
-    run_blocktest_rref(&nvme, 4096, false, false);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/false);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/false);
 
-    run_blocktest_rref(&nvme, 4096, false, true);
-    run_blocktest_rref(&nvme, 4096, false, true);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/true);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/true);
 
 
-    run_blocktest_rref(&nvme, 4096, true, false);
-    run_blocktest_rref(&nvme, 4096, true, false);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/false);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/false);
 
-    run_blocktest_rref(&nvme, 4096, true, true);
-    run_blocktest_rref(&nvme, 4096, true, true);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/true);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/true);
 
     //run_blocktest_raw(&nvme, 30, 32, true);
     //perf_test_raw(&nvme, 10, 32, false);
