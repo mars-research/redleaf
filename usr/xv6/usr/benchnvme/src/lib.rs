905b50a92a103fcfa165d3333948945b2cee185c
Tianjiao Huang <git@tjhu.dev>
2020-05-21 18:18:16 -0600

    benchnvme: init

---
+++ usr/xv6/usr/benchnvme/src/lib.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![forbid(unsafe_code)]
+#![feature(abi_x86_interrupt)]
+#![feature(
+    asm,
+    allocator_api,
+    alloc_layout_extra,
+    alloc_error_handler,
+    const_fn,
+    const_raw_ptr_to_usize_cast,
+    untagged_unions,
+    panic_info_message
+)]
+
+extern crate malloc;
+extern crate alloc;
+
+#[macro_use]
+use alloc::boxed::Box;
+use alloc::string::String;
+use alloc::string::ToString;
+use core::panic::PanicInfo;
+
+use syscalls::{Syscall, Heap};
+use usrlib::{println, print};
+use usr::xv6::Xv6;
+
+#[no_mangle]
+pub fn init(s: Box<dyn Syscall + Send + Sync>, heap: Box<dyn Heap + Send + Sync>, rv6: Box<dyn Xv6>, args: &str) {
+    libsyscalls::syscalls::init(s);
+    rref::init(heap, libsyscalls::syscalls::sys_get_current_domain_id());
+    usrlib::init(rv6.clone());
+    println!("Starting rv6 benchnet with args: {}", args);
+
+    let net = rv6.as_nvme();
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/false);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/false);
+
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/true);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/false, /*is_random=*/true);
+
+
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/false);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/false);
+
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/true);
+    libbenchnvme::run_blocktest_rref(&mut nvme, 4096, /*is_write=*/true, /*is_random=*/true);
+}
+
+
+// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("benchnvme panic: {:?}", info);
+    libsyscalls::syscalls::sys_backtrace();
+    loop {}
+}
