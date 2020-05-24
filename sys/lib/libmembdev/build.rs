ad18d5b7baee9c793ae8507b533f11b42b7eaa5b
Tianjiao Huang <git@tjhu.dev>
2020-05-24 14:40:04 -0600

    membdev: force rebuild if usr changes

--- sys/lib/libmembdev/build.rs
+++ sys/lib/libmembdev/build.rs
@@ -14,6 +14,8 @@ fn main() {
     io::stderr().write_all(&output.stderr).unwrap();
 
     let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}/../../../usr/", manifest_dir);
     println!("cargo:rustc-link-search=native={}/../../../usr/mkfs/build", manifest_dir);
     println!("cargo:rustc-link-lib=static=fs");
 }
