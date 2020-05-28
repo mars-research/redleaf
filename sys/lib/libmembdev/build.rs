35f8240852d48a00e5deeba9ce828bb82abf4ca1
tianjiao <git@tjhu.dev>
2020-05-28 17:23:58 -0500

    Revert "libmembdev: try for force it to rebuild"
    This commit slows down rv6 benchnet over nullnet by 30X
    
    This reverts commit e24936ef00c6dc69d3df18001392f89b9f4a5e6e.

--- sys/lib/libmembdev/build.rs
+++ sys/lib/libmembdev/build.rs
@@ -3,19 +3,19 @@ use std::process::Command;
 use std::io::{self, Write};
 
 fn main() {
-    let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed={}/../../../usr/", manifest_dir);
-    println!("cargo:rustc-link-search=native={}/../../../usr/mkfs/build", manifest_dir);
-
     let output = Command::new("make")
         .arg("-C")
         .arg("../../../usr/mkfs")
         .arg("build/libfs.a")
         .output()
         .unwrap();
+
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
 
+    let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}/../../../usr/", manifest_dir);
+    println!("cargo:rustc-link-search=native={}/../../../usr/mkfs/build", manifest_dir);
     println!("cargo:rustc-link-lib=static=fs");
 }
