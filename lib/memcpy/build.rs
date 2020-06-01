f78bdf03fb1cd3ac67730a28a71568fa97d89956
tianjiao <git@tjhu.dev>
2020-06-01 17:45:22 -0500

    fix musl build

--- lib/memcpy/build.rs
+++ lib/memcpy/build.rs
@@ -4,16 +4,16 @@ use std::process::Command;
 fn main() {
     #[cfg(feature = "musl")]
     {
-        let output = Command::new("make")
-            .arg("-C")
-            .arg("musl")
+        let mut command = Command::new("make");
+        let output = command
+            .args(&["-C", "musl"])
             .arg("ARCH=x86_64")
             .arg("-j")
-            .arg("musl")
+            .arg("lib/libc.a")
             .output()
-            .unwrap()
-            .stdout;
-    
+            .unwrap();
+        assert!(output.status.success(), "Command failed:\n{:?}\n{}", command, String::from_utf8_lossy(&output.stderr));
+
         let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
         println!("cargo:rustc-link-search=native={}/musl/lib", manifest_dir);
         println!("cargo:rustc-link-lib=static=c");
