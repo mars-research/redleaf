ae47b9d3afffe766b938609d13b2ea3c9ed27c6b
Zhaofeng Li <hello@zhaofeng.li>
2020-09-18 22:58:56 -0700

    domains: Uplift unused testtpm domain

--- domains/usr/test/testtpm/src/main.rs
+++ domains/usr/test/testtpm/src/main.rs
@@ -1,27 +1,31 @@
 #![no_std]
-extern crate malloc;
+#![no_main]
 extern crate alloc;
-use libsyscalls;
 use usr::tpm::TpmDev;
+use core::panic::PanicInfo;
+use alloc::boxed::Box;
+
+use console::println;
+use libsyscalls;
 use libtpm::*;
 
 use syscalls::{Syscall, Heap};
 
 #[no_mangle]
-pub fn init(s: Box<dyn Syscall + Send + Sync>, heap: Box<dyn Heap + Send + Sync>, tpm: Box<dyn TpmDev + Send + Sync>) {
+pub fn trusted_entry(s: Box<dyn Syscall + Send + Sync>, heap: Box<dyn Heap + Send + Sync>, tpm: Box<dyn TpmDev + Send + Sync>) {
     libsyscalls::syscalls::init(s);
 
     rref::init(heap, libsyscalls::syscalls::sys_get_current_domain_id());
 
-    println!("Initalizing domain: {}", DOMAIN_NAME);
+    println!("Initalizing domain: testtpm");
 
-    /// Add TPM2 functions here!
+    // Add TPM2 functions here!
     // tpm_test_read_pcr(tpm);
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("domain {} panic: {:?}", DOMAIN_NAME, info);
+    println!("domain testtpm panic: {:?}", info);
     libsyscalls::syscalls::sys_backtrace();
     loop {}
 }
