491a51a84f546b1dfd9088297eb0f1f92b7a4e4e
Tianjiao Huang <git@tjhu.dev>
2020-09-28 17:50:27 -0600

    fix setup script

--- setup.sh
+++ setup.sh
@@ -2,18 +2,18 @@
 # Install Rust toolchain and other dependencies.
 # Change $INSTALL_HOME if you would like Rust to be installed somewhere else.
 # Sample usage:
-#   ./setup.sh INSTALL_HOME=~/large
+#   INSTALL_HOME=~/large ./setup.sh 
 
 echo "Setting up Rust build environemnt for RedLeaf"
 
 USR_HOME=`printenv HOME`
-INSTALL_HOME=$USR_HOME # Default install path is $HOME. 
+INSTALL_HOME?=$USR_HOME # Default install path is $HOME. 
 CARGO_HOME=$INSTALL_HOME/.cargo
-RUSTUP_HOME= $INSTALL_HOME/.rustup
+RUSTUP_HOME=$INSTALL_HOME/.rustup
 RUST_HOME=$CARGO_HOME/bin
 
 echo $RUST_HOME
-curl https://sh.rustup.rs -sSf | bash -s -- --default-toolchain nightly-2020-08-22 -y
+curl https://sh.rustup.rs -sSf | CARGO_HOME=$CARGO_HOME RUSTUP_HOME=$RUSTUP_HOME bash -s -- --default-toolchain nightly-2020-08-22 -y
 $RUST_HOME/rustup component add llvm-tools-preview rust-src
 $RUST_HOME/cargo install stack-sizes
 
@@ -22,4 +22,4 @@ sudo apt-get update
 sudo apt-get install qemu nasm grub-pc-bin xorriso numactl -y
 
 echo "To get started you need Cargo's bin directory ($CARGO_HOME/bin) in your PATH environment variable. Next time you log in this will be done automatically."
-echo "To configure your current shell run `source $CARGO_HOME/env`"
+echo "To configure your current shell run 'source $CARGO_HOME/env'"
