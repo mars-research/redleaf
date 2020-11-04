f10fe3c309a47ca1dc6d43d05f21a8a5e56cd59f
Tianjiao Huang <git@tjhu.dev>
2020-11-03 17:43:34 -0800

    proxy: use generate_trampoline for one_arg; looks like it's working?

--- domains/usr/proxy/src/gen.rs
+++ domains/usr/proxy/src/gen.rs
@@ -814,32 +814,7 @@ extern {
 
 trampoline!(no_arg);
 
-/* 
- * Code to unwind one_arg
- */
-
-#[no_mangle]
-pub extern fn one_arg(s: &Box<dyn usr::dom_c::DomC>, x: usize) -> RpcResult<usize> {
-    //println!("one_arg: x:{}", x);
-    s.one_arg(x)
-}
-
-#[no_mangle]
-pub extern fn one_arg_err(s: &Box<dyn usr::dom_c::DomC>, x: usize) -> RpcResult<usize> {
-    println!("one_arg was aborted, x:{}", x);
-    Err(unsafe{RpcError::panic()})
-}
-
-#[no_mangle]
-pub extern "C" fn one_arg_addr() -> u64 {
-    one_arg_err as u64
-}
-
-extern {
-    fn one_arg_tramp(s: &Box<dyn usr::dom_c::DomC>, x: usize) -> RpcResult<usize>;
-}
-
-trampoline!(one_arg);
+::codegen_lib::generate_trampoline!(s: &Box<dyn usr::dom_c::DomC>, one_arg(x: usize) -> RpcResult<usize>);
 
 /* 
  * Code to unwind one_rref
