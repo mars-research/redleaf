c9ffcdeb0edaac1b6018435b3352c98b465f6b94
Zhaofeng Li <hello@zhaofeng.li>
2020-12-28 17:22:28 -0800

    kernel: Allocator fixes

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -4,13 +4,14 @@ use x86::bits64::paging;
 use core::mem::transmute;
 use core::alloc::{GlobalAlloc, Layout};
 use spin::Mutex;
-use slabmalloc_kernel::{ObjectPage, PageProvider, ZoneAllocator};
-use crate::memory::buddy::BUDDY;
-use log::{trace};
+use slabmalloc::{Allocator, ZoneAllocator, AllocationError};
+use crate::memory::buddy::{BUDDY, BuddyFrameAllocator};
+use log::trace;
 use crate::arch::KERNEL_END;
+use core::ptr::NonNull;
 
 pub use self::buddy::BuddyFrameAllocator as PhysicalMemoryAllocator;
-pub use crate::arch::memory::{paddr_to_kernel_vaddr, PAddr, VAddr, BASE_PAGE_SIZE};
+pub use crate::arch::memory::{kernel_vaddr_to_paddr, paddr_to_kernel_vaddr, PAddr, VAddr, BASE_PAGE_SIZE};
 
 pub mod buddy;
 
@@ -118,179 +119,20 @@ impl fmt::Debug for Frame {
     }
 }
 
-pub trait PageTableProvider<'a> {
-    fn allocate_pml4<'b>(&mut self) -> Option<&'b mut paging::PML4>;
-    fn new_pdpt(&mut self) -> Option<paging::PML4Entry>;
-    fn new_pd(&mut self) -> Option<paging::PDPTEntry>;
-    fn new_pt(&mut self) -> Option<paging::PDEntry>;
-    fn new_page(&mut self) -> Option<paging::PTEntry>;
-}
-
 #[allow(dead_code)]
-pub struct BespinPageTableProvider;
+pub struct SafeZoneAllocator {
+    allocator: Mutex<ZoneAllocator<'static>>,
 
-impl BespinPageTableProvider {
-    #[allow(dead_code)]
-    pub const fn new() -> BespinPageTableProvider {
-        BespinPageTableProvider
-    }
+    // Should be dyn PhysicalAllocator, but let's not involve dynamic dispatch
+    buddy: &'static Mutex<Option<BuddyFrameAllocator>>,
 }
-
-impl<'a> PageTableProvider<'a> for BespinPageTableProvider {
-    /// Allocate a PML4 table.
-    fn allocate_pml4<'b>(&mut self) -> Option<&'b mut paging::PML4> {
-        if let Some(ref mut fmanager) = *BUDDY.lock() {
-            unsafe {
-                let f = fmanager.allocate(
-                    Layout::new::<paging::Page>()
-                        .align_to(BASE_PAGE_SIZE)
-                        .unwrap(),
-                );
-                f.map(|frame| {
-                    let pml4: &'b mut [paging::PML4Entry; 512] =
-                        transmute(paddr_to_kernel_vaddr(frame.base));
-                    pml4
-                })
-            }
-        } else {
-            panic!("__rust_allocate: buddy not initialized");
-        }
-    }
-
-    /// Allocate a new page directory and return a PML4 entry for it.
-    fn new_pdpt(&mut self) -> Option<paging::PML4Entry> {
-        if let Some(ref mut fmanager) = *BUDDY.lock() {
-            unsafe {
-                fmanager
-                    .allocate(
-                        Layout::new::<paging::Page>()
-                            .align_to(BASE_PAGE_SIZE)
-                            .unwrap(),
-                    )
-                    .map(|frame| {
-                        paging::PML4Entry::new(
-                            frame.base,
-                            paging::PML4Flags::P | paging::PML4Flags::RW | paging::PML4Flags::US,
-                        )
-                    })
-            }
-        } else {
-            panic!("__rust_allocate: buddy not initialized");
-        }
-    }
-
-    /// Allocate a new page directory and return a pdpt entry for it.
-    fn new_pd(&mut self) -> Option<paging::PDPTEntry> {
-        if let Some(ref mut fmanager) = *BUDDY.lock() {
-            unsafe {
-                fmanager
-                    .allocate(
-                        Layout::new::<paging::Page>()
-                            .align_to(BASE_PAGE_SIZE)
-                            .unwrap(),
-                    )
-                    .map(|frame| {
-                        paging::PDPTEntry::new(
-                            frame.base,
-                            paging::PDPTFlags::P | paging::PDPTFlags::RW | paging::PDPTFlags::US,
-                        )
-                    })
-            }
-        } else {
-            panic!("__rust_allocate: buddy not initialized");
-        }
-    }
-
-    /// Allocate a new page-directory and return a page directory entry for it.
-    fn new_pt(&mut self) -> Option<paging::PDEntry> {
-        if let Some(ref mut fmanager) = *BUDDY.lock() {
-            unsafe {
-                fmanager
-                    .allocate(
-                        Layout::new::<paging::Page>()
-                            .align_to(BASE_PAGE_SIZE)
-                            .unwrap(),
-                    )
-                    .map(|frame| {
-                        paging::PDEntry::new(
-                            frame.base,
-                            paging::PDFlags::P | paging::PDFlags::RW | paging::PDFlags::US,
-                        )
-                    })
-            }
-        } else {
-            panic!("__rust_allocate: buddy not initialized");
-        }
-    }
-
-    /// Allocate a new (4KiB) page and map it.
-    fn new_page(&mut self) -> Option<paging::PTEntry> {
-        if let Some(ref mut fmanager) = *BUDDY.lock() {
-            unsafe {
-                fmanager
-                    .allocate(
-                        Layout::new::<paging::Page>()
-                            .align_to(BASE_PAGE_SIZE)
-                            .unwrap(),
-                    )
-                    .map(|frame| {
-                        paging::PTEntry::new(
-                            frame.base,
-                            paging::PTFlags::P | paging::PTFlags::RW | paging::PTFlags::US,
-                        )
-                    })
-            }
-        } else {
-            panic!("__rust_allocate: buddy not initialized");
-        }
-    }
-}
-pub struct BespinSlabsProvider;
-
-unsafe impl Send for BespinSlabsProvider {}
-unsafe impl Sync for BespinSlabsProvider {}
-
-impl BespinSlabsProvider {
-    pub const fn new() -> BespinSlabsProvider {
-        BespinSlabsProvider
-    }
-}
-
-impl<'a> PageProvider<'a> for BespinSlabsProvider {
-    fn allocate_page(&mut self) -> Option<&'a mut ObjectPage<'a>> {
-        if let Some(ref mut fmanager) = *BUDDY.lock() {
-            let f = unsafe {
-                fmanager.allocate(
-                    Layout::new::<paging::Page>()
-                        .align_to(BASE_PAGE_SIZE)
-                        .unwrap(),
-               )
-            };
-            f.map(|mut frame| unsafe {
-                frame.zero();
-                trace!("slabmalloc allocate frame.base = {:x}", frame.base);
-                let sp: &'a mut ObjectPage = transmute(paddr_to_kernel_vaddr(frame.base));
-                sp
-            })
-        } else {
-            panic!("__rust_allocate: buddy not initialized");
-        }
-    }
-
-    fn release_page(&mut self, _p: &'a mut ObjectPage<'a>) {
-        //trace_alloc!("TODO!");
-    }
-}
-
-#[allow(dead_code)]
-static PAGER: Mutex<BespinSlabsProvider> = Mutex::new(BespinSlabsProvider::new());
-
-#[allow(dead_code)]
-pub struct SafeZoneAllocator(Mutex<ZoneAllocator<'static>>);
 
 impl SafeZoneAllocator {
-    pub const fn new(provider: &'static Mutex<dyn PageProvider>) -> SafeZoneAllocator {
-        SafeZoneAllocator(Mutex::new(ZoneAllocator::new(provider)))
+    pub const fn new(buddy: &'static Mutex<Option<BuddyFrameAllocator>>) -> SafeZoneAllocator {
+        Self {
+            allocator: Mutex::new(ZoneAllocator::new()),
+            buddy,
+        }
     }
 }
 
@@ -302,58 +144,93 @@ fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
 unsafe impl GlobalAlloc for SafeZoneAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         trace!("alloc layout={:?}", layout);
-        if layout.size() <= ZoneAllocator::MAX_ALLOC_SIZE {
-            let ptr = self.0.lock().allocate(layout);
-            trace!("allocated ptr=0x{:x} layout={:?}", ptr as usize, layout);
-            ptr
-        } else {
-            let ptr; // = core::ptr::null_mut();
 
-            if let Some(ref mut fmanager) = *BUDDY.lock() {
-                let f = fmanager.allocate(layout);
-                match f {
-                    Some(_frame) => trace_alloc!("Alloc {:x?}", frame),
-                    _ => { },
-                };
-                ptr = f.map_or(core::ptr::null_mut(), |mut region| {
-                    region.zero();
-                    region.kernel_vaddr().as_mut_ptr()
-                });
-                trace!("allocated ptr=0x{:x} layout={:?}", ptr as usize, layout);
-            } else {
-                panic!("__rust_allocate: buddy not initialized");
+        let mut buddy = self.buddy.lock();
+        let mut buddy = buddy.as_mut()
+            .expect("__rust_allocate: Buddy is not initialized");
+
+        // Let's just pass most stuff to buddy I guess?
+        match layout.size() {
+            BASE_PAGE_SIZE => {
+                // Use buddy directly
+                let mut frame = buddy.allocate(layout)
+                    .expect("__rust_allocate: Out of memory");
+
+                frame.zero();
+                frame.kernel_vaddr().as_mut_ptr()
             }
-            ptr
+            0..=ZoneAllocator::MAX_ALLOC_SIZE => {
+                // Ask zone allocator
+                println!("Using zoneallocator");
+                let mut zone_allocator = self.allocator.lock();
+                match zone_allocator.allocate(layout) {
+                    Ok(nptr) => nptr.as_ptr(),
+                    Err(AllocationError::OutOfMemory) => {
+                        // Allocator is hungry and needs new pages from
+                        // the buddy :P
+                        let mut frame = buddy.allocate(layout)
+                            .expect("__rust_allocate: Out of memory (ZoneAllocator)");
+
+                        frame.zero();
+                        frame.kernel_vaddr().as_mut_ptr()
+                    }
+                    Err(AllocationError::InvalidLayout) => {
+                        panic!("__rust_allocate: Invalid layout size (ZoneAllocator)");
+                    }
+                }
+            }
+            _ => {
+                // Use buddy directly
+                let mut frame = buddy.allocate(layout)
+                    .expect("__rust_allocate: Out of memory");
+
+                frame.zero();
+                frame.kernel_vaddr().as_mut_ptr()
+            }
         }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        // FIXME: ZoneAllocator dealloc
+        return;
+
         trace!("dealloc ptr = 0x{:x} layout={:?}", ptr as usize, layout);
-        if layout.size() <= ZoneAllocator::MAX_ALLOC_SIZE {
-            //debug!("dealloc ptr = 0x{:x} layout={:?}", ptr as usize, layout);
-            self.0.lock().deallocate(ptr, layout);
-        } else {
-            use crate::arch::memory::{kernel_vaddr_to_paddr};
-            if let Some(ref mut fmanager) = *BUDDY.lock() {
-                let frame = Frame::new(
-                        kernel_vaddr_to_paddr(VAddr::from_u64(ptr as u64)),
-                        layout.size());
+        let mut buddy = self.buddy.lock();
+        let mut buddy = buddy.as_mut()
+            .expect("__rust_allocate: Buddy is not initialized");
 
-                trace_alloc!("Dealloc {:x?}", frame);
+        let vaddr = VAddr::from_usize(ptr as usize);
+        let frame = Frame::new(
+            kernel_vaddr_to_paddr(vaddr),
 
-                fmanager.deallocate(
-                    frame,
-                    layout,
-                );
-            } else {
-                panic!("__rust_allocate: buddy not initialized");
+            // As long as the layout is the same, we will be fine
+            0,
+        );
+
+        // Let's just pass most stuff to buddy I guess?
+        match layout.size() {
+            BASE_PAGE_SIZE => {
+                buddy.deallocate(frame, layout);
+            }
+            0..=ZoneAllocator::MAX_ALLOC_SIZE => {
+                // Ask zone allocator
+                if let Some(nptr) = NonNull::new(ptr) {
+                    let mut zone_allocator = self.allocator.lock();
+                    zone_allocator.deallocate(nptr, layout)
+                        .expect("__rust_deallocate: Failed to deallocate (ZoneAllocator)");
+                } else {
+                }
+            }
+            _ => {
+                // Use buddy directly
+                buddy.deallocate(frame, layout);
             }
         }
     }
 }
 
 #[global_allocator]
-pub static MEM_PROVIDER: SafeZoneAllocator = SafeZoneAllocator::new(&PAGER);
+pub static MEM_PROVIDER: SafeZoneAllocator = SafeZoneAllocator::new(&BUDDY);
 
 use crate::arch::vspace::{VSpace, MapAction};
 use x86::controlregs;
