cb75e0be10ec8858e2f2c94c88694515b5181e31
Tianjiao Huang <git@tjhu.dev>
2021-07-09 13:46:16 -0700

    creates the possibility that dom_c can call itself

--- interface/src/dom_c.rs
+++ interface/src/dom_c.rs
@@ -2,9 +2,12 @@ use crate::rref::RRef;
 
 use crate::rpc::RpcResult;
 
+use alloc::boxed::Box;
+
 #[interface]
 pub trait DomC {
     fn no_arg(&self) -> RpcResult<()>;
     fn one_arg(&self, x: usize) -> RpcResult<usize>;
     fn one_rref(&self, x: RRef<usize>) -> RpcResult<RRef<usize>>;
+    fn init_dom_c(&self, c: Box<dyn DomC>) -> RpcResult<()>;
 }
